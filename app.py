1a59343fc5f08528d066c306da401d4fa6a8b584
Briana Rajan <briana@Brianas-MacBook-Air.local>
2024-04-07 00:51:49 -0700

    zip to coord

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 from model import calculation
+from zip_to_coord import ZipToCoord
+import ee
+import os
 
 app = Flask(__name__)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "crypto-pulsar-418018-70323bd4b67f.json"
 
+ee.Initialize()
 @app.route("/")
 def home():
     return render_template('home.html')
@@ -13,7 +18,9 @@ def process_form():
     location=request.form.get("location")
     landsize=request.form.get("landsize")
     print(location,budget,landsize)
-    location,result_value=calculation(budget,location,landsize)
+    #changing location to coord
+    coord = ZipToCoord(location)
+    location,result_value=calculation(budget,coord,landsize)
     return render_template("result.html", location=location, result_value=result_value)
 if __name__ == '__main__':
     app.run(debug=True)
